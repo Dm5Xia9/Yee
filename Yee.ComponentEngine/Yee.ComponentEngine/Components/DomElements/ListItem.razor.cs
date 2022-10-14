06f8ede61508d853824738dfcd015c9adf3eaa36
DESKTOP-HRIAJFV\mikha <mikhail-bulatov@bk.ru>
2022-10-14 16:15:30 +0500

    add components engine

---
+++ Yee.ComponentEngine/Yee.ComponentEngine/Components/DomElements/ListItem.razor.cs
@@ -0,0 +1,13 @@
+﻿using Yee.ComponentEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yee.ComponentEngine.Components.DomElements
+{
+    public partial class ListItem : BaseDomElement
+    {
+    }
+}
