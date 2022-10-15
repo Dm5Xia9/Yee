655c73a71bc6b969a2c843ea5ed58d8fe4c4b564
Dima5x9 <dimastd333@gmail.com>
2022-10-15 22:14:13 +0500

    теги в модулях

---
+++ Yee.Admin/Helpers/ColorHelper.cs
@@ -0,0 +1,33 @@
+﻿using AntDesign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yee.Admin.Helpers
+{
+    public static class ColorHelper
+    {
+        private static Dictionary<string, PresetColor> _namesFromColors = new Dictionary<string, PresetColor>();
+
+
+        public static PresetColor GetColorFromName(string name)
+        {
+            if (_namesFromColors.ContainsKey(name))
+            {
+                return _namesFromColors[name];
+            }
+
+
+            var vals = Enum.GetValues(typeof(PresetColor));
+            var values = vals
+                .OfType<PresetColor>().Where(p => !_namesFromColors.Values.Contains(p));
+            Random random = new Random();
+            PresetColor randomcolor = (PresetColor)vals.GetValue(random.Next(values.Count()));
+
+            _namesFromColors.Add(name, randomcolor);
+            return randomcolor;
+        }
+    }
+}
